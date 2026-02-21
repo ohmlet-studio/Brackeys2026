9cf51745e25a70b2125b91735bc05e36744f63cb
RubenGres <ruben.gres@proton.me>
2026-02-21 02:26:23 +0100

    final scne working <3 xoxoxo

--- project/scripts/final_scene.gd
+++ project/scripts/final_scene.gd
@@ -1,16 +1,18 @@
 extends Control
 
-@onready var objMesh1: MeshInstance3D = $SubViewportContainer/SubViewport/Node3D/Object1
-@onready var objMesh2: MeshInstance3D = $SubViewportContainer/SubViewport/Node3D/Object2
-@onready var objMesh3: MeshInstance3D = $SubViewportContainer/SubViewport/Node3D/Object3
+@export var pickable_slot: Pickable
+@export var pickable_slot2: Pickable
+@export var pickable_slot3: Pickable
+@onready var slots = [$SubViewportContainer/SubViewport/Node3D/Slot, $SubViewportContainer/SubViewport/Node3D/Slot2, $SubViewportContainer/SubViewport/Node3D/Slot3]
 
 func _ready() -> void:
-	var scene: Node3D = load("res://assets/models/memory_objects/carnet.fbx").instantiate() as Node3D
-	var src_mi: MeshInstance3D = scene.get_child(0) as MeshInstance3D
-	var source_mesh: Mesh = src_mi.mesh
-	objMesh1.mesh = source_mesh.duplicate() as Mesh
-	
-	scene = load("res://assets/models/memory_objects/lighter.fbx").instantiate() as Node3D
-	src_mi = scene.get_child(0) as MeshInstance3D
-	source_mesh = src_mi.mesh
-	objMesh2.mesh = source_mesh.duplicate() as Mesh
+	var slot_id = 0
+	for memory in [pickable_slot, pickable_slot2, pickable_slot3]:
+		if not memory:
+			continue
+		 
+		var path = memory.object_to_scan.resource_path
+		var object_room1: Node3D = load(path).instantiate()
+		object_room1.scale = Vector3.ONE * memory.inspect_scale
+		slots[slot_id].add_child(object_room1)
+		slot_id += 1
